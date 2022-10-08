749c70c3d90a9ac69a87c38d1c4877b88a3e4dc3
srisht <fateh321@gmail.com>
2022-10-08 03:31:04 -0400

    test

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-node token_a_node_0_0.js & node token_a_node_1.js & node token_a_node_2.js & node token_a_node_3.js & node token_a_node_4.js & node token_a_node_5.js & node token_a_node_6.js & node token_a_node_7.js &
+node token_a_node_0_0.js & node token_a_node_1.js & node token_a_node_2.js & node token_a_node_3.js &
+node token_a_node_4.js & node token_a_node_5.js & node token_a_node_6.js & node token_a_node_7.js &
